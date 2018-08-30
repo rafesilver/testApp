b402076f6d4012555507bb7f3e6124a4b8fc3f98
Rafael Silveira <rafesilver@users.noreply.github.com>
2018-08-30 13:54:32 -0500

    Change Expenses to Persons

--- index.php
+++ index.php
@@ -49,8 +49,8 @@
 	}
 
 	// Extract previously-selected Month and Year
-	$prevExpenseMonth= $prevSelections['prevExpenseMonth'];
-	$prevExpenseYear= $prevSelections['prevExpenseYear'];
+	$prevStartDate= $prevSelections['prevStartDate'];
+	$prevEndDate= $prevSelections['prevEndDate'];
 ?>
 
 <div class="intro">
@@ -68,54 +68,65 @@ The PHP script insertToDb.php will be executed after the user clicks "Submit"-->
 <div class="container">
 	<form action="insertToDb.php" method="post">
 
-		<label>Expense Day (1-31):</label>
-		<input type="number" step="1" name="expense_day" required>
+		<label>Start Date:</label>
+		<input type="text" step="1" name="start_date" required>
+
+		<label>End Date:</label>
+		<input type="text" step="1" name="end_date" required>
+
+		<label>Vehicle Make:</label>
+		<input type="text" step="1" name="vehicle_make" required>
+
+		<label>Vehicle Model:</label>
+		<input type="text" step="1" name="vehicle_model" required>
+
+		<label>Employee Name:</label>
+		<input type="text" step="1" name="employee_name" required>
 
 		<!-- Dropdown menu for expense month, remembering previously selected month -->
-		<label>Expense Month</label>
+		<!-- <label>Expense Month</label>
 		<select name="expense_month">
 			<option value="-1">Month:</option>
-			<option value="01"<?php echo $prevExpenseMonth == 1 ? 'selected="selected"' : ''; ?>>Jan</option>
-			<option value="02"<?php echo $prevExpenseMonth == 2 ? 'selected="selected"' : ''; ?>>Feb</option>
-			<option value="03"<?php echo $prevExpenseMonth == 3 ? 'selected="selected"' : ''; ?>>Mar</option>
-			<option value="04"<?php echo $prevExpenseMonth == 4 ? 'selected="selected"' : ''; ?>>Apr</option>
-			<option value="05"<?php echo $prevExpenseMonth == 5 ? 'selected="selected"' : ''; ?>>May</option>
-			<option value="06"<?php echo $prevExpenseMonth == 6 ? 'selected="selected"' : ''; ?>>Jun</option>
-			<option value="07"<?php echo $prevExpenseMonth == 7 ? 'selected="selected"' : ''; ?>>Jul</option>
-			<option value="08"<?php echo $prevExpenseMonth == 8 ? 'selected="selected"' : ''; ?>>Aug</option>
-			<option value="09"<?php echo $prevExpenseMonth == 9 ? 'selected="selected"' : ''; ?>>Sep</option>
-			<option value="10"<?php echo $prevExpenseMonth == 10 ? 'selected="selected"' : ''; ?>>Oct</option>
-			<option value="11"<?php echo $prevExpenseMonth == 11 ? 'selected="selected"' : ''; ?>>Nov</option>
-			<option value="12"<?php echo $prevExpenseMonth == 12 ? 'selected="selected"' : ''; ?>>Dec</option>
-		</select><br>
+			<option value="01"<?php //echo $prevExpenseMonth == 1 ? 'selected="selected"' : ''; ?>>Jan</option>
+			<option value="02"<?php //echo $prevExpenseMonth == 2 ? 'selected="selected"' : ''; ?>>Feb</option>
+			<option value="03"<?php //echo $prevExpenseMonth == 3 ? 'selected="selected"' : ''; ?>>Mar</option>
+			<option value="04"<?php //echo $prevExpenseMonth == 4 ? 'selected="selected"' : ''; ?>>Apr</option>
+			<option value="05"<?php //echo $prevExpenseMonth == 5 ? 'selected="selected"' : ''; ?>>May</option>
+			<option value="06"<?php //echo $prevExpenseMonth == 6 ? 'selected="selected"' : ''; ?>>Jun</option>
+			<option value="07"<?php //echo $prevExpenseMonth == 7 ? 'selected="selected"' : ''; ?>>Jul</option>
+			<option value="08"<?php //echo $prevExpenseMonth == 8 ? 'selected="selected"' : ''; ?>>Aug</option>
+			<option value="09"<?php //echo $prevExpenseMonth == 9 ? 'selected="selected"' : ''; ?>>Sep</option>
+			<option value="10"<?php //echo $prevExpenseMonth == 10 ? 'selected="selected"' : ''; ?>>Oct</option>
+			<option value="11"<?php //echo $prevExpenseMonth == 11 ? 'selected="selected"' : ''; ?>>Nov</option>
+			<option value="12"<?php //echo $prevExpenseMonth == 12 ? 'selected="selected"' : ''; ?>>Dec</option>
+		</select><br> -->
 
 		<!-- Text input for year, remembering previously selected year -->
-		<label>Expense Year (YYYY):</label>
-		<input type="number" step="1" name="expense_year" value="<?php echo $prevExpenseYear;  ?>" required><br>
+		<!-- <label>Expense Year:</label>
+		<input type="number" step="1" name="expense_year" value="<?php echo $prevExpenseYear;  ?>" required><br> -->
  
-		<label>Expense Amount (US$):</label>
-		<input type="number" step="0.01" name="expense_amount" required><br>
+		<!-- <label>Expense Amount (US$):</label>
+		<input type="number" step="0.01" name="expense_amount" required><br> -->
 
-		<label>Expense Category:</label>
+		<!-- <label>Expense Category:</label>
 		<select name="expense_category">
 			<option value="-1">Category:</option>
-			<?php echo " . $options . " ?>
-		</select><br>
+			<?php //echo " . $options . " ?>
+		</select><br> -->
 
-		<label>Notes (optional) [no accents or tildes]:</label>
-		<input type="text" name="input_note" ><br>
+		<!-- <label>Notes (optional) [no accents or tildes]:</label>
+		<input type="text" name="input_note" ><br> -->
 
 		<button type="submit">Submit</button>
 	</form>
 </div>
 
 <h3> Previous Input (if any) - for verification purposes:</h3>
-<p> Expense Day: <?php echo $prevSelections['prevExpenseDay'] ?> </p>
-<p> Expense Month: <?php echo $prevSelections['prevExpenseMonth'] ?> </p>
-<p> Expense Year: <?php echo $prevSelections['prevExpenseYear'] ?> </p>
-<p> Expense Category: <?php echo $prevSelections['prevExpenseCategory'] ?> </p>
-<p> Expense Amount: <?php echo $prevSelections['prevExpenseAmount'] ?> </p>
-<p> Expense Note: <?php echo $prevSelections['prevExpenseNote'] ?> </p>
+<p> Start Date: <?php echo $prevSelections['prevStartDate'] ?> </p>
+<p> End Date: <?php echo $prevSelections['prevEndDate'] ?> </p>
+<p> Vehicle Make: <?php echo $prevSelections['prevVehicleMake'] ?> </p>
+<p> Vehicle Model: <?php echo $prevSelections['prevVehicleModel'] ?> </p>
+<p> Employee Name: <?php echo $prevSelections['prevEmployeeName'] ?> </p>
 <p> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </p>
 
 </body>
--- insertToDb.php
+++ insertToDb.php
@@ -10,10 +10,10 @@
 	}
 
 	// Define function to check that inputted expense number has a maximum of 2 decimal places
-	function validateTwoDecimals($number)
-	{
-	   return (preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $number));
-	}
+	// function validateTwoDecimals($number)
+	// {
+	//    return (preg_match('/^[0-9]+(\.[0-9]{1,2})?$/', $number));
+	// }
  
 	// PHP script used to connect to backend Azure SQL database
 	require 'ConnectToDatabase.php';
@@ -22,26 +22,27 @@
 	session_start();
 
 	// Define ariables and set to empty values
-	$expenseDay = $expenseMonth = $expenseYear = $expenseAmount = $expenseNote = $expenseCategory = $errorMessage = NULL;
+	$startDate = $endDate = $vehicleMake = $vehicleModel = $employeeName = $errorMessage = NULL;
 
 	// Get input variables
-	$expenseDay= (int) parse_input($_POST['expense_day']);
-	$expenseMonth= (int) parse_input($_POST['expense_month']);
-	$expenseYear= (int) parse_input($_POST['expense_year']);
-	$expenseAmount= (float) parse_input($_POST['expense_amount']);
-	$expenseNote= parse_input($_POST['input_note']);
-	$expenseCategory= parse_input($_POST['expense_category']);
+	$startDate= parse_input($_POST['start_date']);
+	$endDate= parse_input($_POST['end_date']);
+	// $startDate = $endDate = $vehicleMake = $vehicleModel = $employeeName = $errorMessage = NULL;
+	$vehicleMake= parse_input($_POST['vehicle_make']);
+	$vehicleModel= parse_input($_POST['vehicle_model']);
+	$employeeName= parse_input($_POST['employee_name']);
+
 
 	// Get the authentication claims stored in the Token Store after user logins using Azure Active Directory
-	$claims= json_decode($_SERVER['MS_CLIENT_PRINCIPAL'])->claims;
-	foreach($claims as $claim)
-	{		
-		if ( $claim->typ == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress" )
-		{
-			$userEmail= $claim->val;
-			break;
-		}
-	}
+	// $claims= json_decode($_SERVER['MS_CLIENT_PRINCIPAL'])->claims;
+	// foreach($claims as $claim)
+	// {		
+	// 	if ( $claim->typ == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress" )
+	// 	{
+	// 		$userEmail= $claim->val;
+	// 		break;
+	// 	}
+	// }
 
 	///////////////////////////////////////////////////////
 	//////////////////// INPUT VALIDATION /////////////////
@@ -50,17 +51,10 @@
 	//Initialize variable to keep track of any errors
 	$anyErrors= FALSE;
 
-	// Check category validity
-	if ($expenseCategory == '-1') {$errorMessage= "Error: Invalid Category Selected"; $anyErrors= TRUE;}
 	
 	// Check date validity
-	$isValidDate= checkdate($expenseMonth, $expenseDay, $expenseYear);
-	if (!$isValidDate) {$errorMessage= "Error: Invalid Date"; $anyErrors= TRUE;}
-
-	// Check that the expense amount input has maximum of 2 decimal places (check against string input, not the float parsed input)
-	$isValidExpenseAmount= validateTwoDecimals(parse_input($_POST['expense_amount']));
-	if (!$isValidExpenseAmount) {$errorMessage= "Error: Invalid Expense Amount"; $anyErrors= TRUE;}
-
+	// $isValidDate= checkdate($expenseMonth, $expenseDay, $expenseYear);
+	// if (!$isValidDate) {$errorMessage= "Error: Invalid Date"; $anyErrors= TRUE;}
 
 	///////////////////////////////////////////////////////
 	////////// INPUT PARSING AND WRITE TO SQL DB //////////
@@ -70,40 +64,32 @@
 	if ( !$anyErrors ) 
 	{
 		// Create a DateTime object based on inputted data
-		$dateObj= DateTime::createFromFormat('Y-m-d', $expenseYear . "-" . $expenseMonth . "-" . $expenseDay);
+		// $startDateObj= DateTime::createFromFormat('Y-m-d', $startYear . "-" . $startMonth . "-" . $startDay);
 
-		// Get the name of the month (e.g. January) of this expense
-		$expenseMonthName= $dateObj->format('F');
+		// // Get the name of the month (e.g. January) of this expense
+		// $expenseMonthName= $startDateObj->format('F');
 
-		// Get the day of the week (e.g. Tuesday) of this expense
-		$expenseDayOfWeekNum= $dateObj->format('w');
-		$days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday');
-		$expenseDayOfWeek = $days[$expenseDayOfWeekNum];
+		// // Get the day of the week (e.g. Tuesday) of this expense
+		// $expenseDayOfWeekNum= $startDateObj->format('w');
+		// $days = array('Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday','Friday', 'Saturday');
+		// $expenseDayOfWeek = $days[$expenseDayOfWeekNum];
 
 		// Connect to Azure SQL Database
 		$conn = ConnectToDabase();
 
 		// Build SQL query to insert new expense data into SQL database
 		$tsql=
-		"INSERT INTO Expenses (	
-				UserName,
-				ExpenseDay,
-				ExpenseDayOfWeek,
-				ExpenseMonth,
-				ExpenseMonthName,
-				ExpenseYear,
-				ExpenseCategory,
-				ExpenseAmount,
-				Notes)
-		VALUES ('" . $userEmail . "',
-				'" . $expenseDay . "', 
-				'" . $expenseDayOfWeek . "', 
-				'" . $expenseMonth . "', 
-				'" . $expenseMonthName . "', 
-				'" . $expenseYear . "', 
-				'" . $expenseCategory . "', 
-				'" . $expenseAmount . "', 
-				'" . $expenseNote . "')";
+		"INSERT INTO Persons (	
+				StartDate,
+				EndDate,
+				VehicleMake,
+				VehicleModel,
+				EmployeeName)
+		VALUES ('" . $startDate . "',
+				'" . $endDate . "', 
+				'" . $vehicleMake . "', 
+				'" . $vehicleModel . "', 
+				'" . $employeeName . "')";
 
 		// Run query
 		$sqlQueryStatus= sqlsrv_query($conn, $tsql);
@@ -117,12 +103,12 @@
 
 	// Populate array with key-value pairs
 	$prevSelections['errorMessage']= $errorMessage;
-	$prevSelections['prevExpenseDay']= $expenseDay;
-	$prevSelections['prevExpenseMonth']= $expenseMonth;
-	$prevSelections['prevExpenseYear']= $expenseYear;
-	$prevSelections['prevExpenseCategory']= $expenseCategory;
-	$prevSelections['prevExpenseAmount']= $expenseAmount;
-	$prevSelections['prevExpenseNote']= $expenseNote;
+	$prevSelections['prevStartDate']= $startDate;
+	$prevSelections['prevEndDate']= $endDate;
+	$prevSelections['prevVehicleMake']= $vehicleMake;
+	$prevSelections['prevVehicleModel']= $vehicleModel;
+	$prevSelections['prevEmployeeName']= $employeeName;
+
 
 	// Store previously-selected data as part of info to carry over after URL redirection
 	$_SESSION['prevSelections'] = $prevSelections;
